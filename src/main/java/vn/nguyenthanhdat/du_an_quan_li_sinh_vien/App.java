e412a3a4433445400fc0c112a3d939bb9bf879a1
Nguyen Thanh Dat <3004nguyenthanhdat@gmail.com>
2026-01-10 02:11:17 +0700

    init project

---
+++ src/main/java/vn/nguyenthanhdat/du_an_quan_li_sinh_vien/App.java
@@ -0,0 +1,15 @@
+package vn.nguyenthanhdat.du_an_quan_li_sinh_vien;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        for(int i=1; i<=100; i++) {
+            System.out.println("Da! " + i);
+        }
+    }
+}
